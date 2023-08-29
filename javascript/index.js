ca02f90ea9e55728c2170147c6c9dae5c64bae3a
David John <david.john@mail.utoronto.ca>
2023-08-29 14:15:05 -0400

    Added cards for software projects instead of previous column layout

--- javascript/index.js
+++ javascript/index.js
@@ -385,6 +385,33 @@ function ifKeyDivsAreOnScreen() {
 }
 
 
+/*##################################################*/
+/* Effects and Animations  */
+/*##################################################*/
+
+$('.RevealCardPrjt').hover(
+  function () { // Mouse enter
+      $(this).find(".revealImg").removeClass("imgClassReveal");
+      
+      $(this).find(".revealImg").addClass("imgClassHide");
+      
+      $(this).find(".revealDes").removeClass("descriptionClassHide");
+      
+      $(this).find(".revealDes").addClass("descriptionClassReveal");
+
+  }, function () { // mouse leave
+      $(this).find(".revealImg").addClass("imgClassReveal");
+      
+      $(this).find(".revealImg").removeClass("imgClassHide");
+      
+      $(this).find(".revealDes").addClass("descriptionClassHide");
+      
+      $(this).find(".revealDes").removeClass("descriptionClassReveal");
+      
+  }
+);
+
+
 
 
 
